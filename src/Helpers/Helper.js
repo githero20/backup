db5b463bd6d83d0f5f8d674ab289a14f962801f2
olabiranj <olabiranj@gmail.com>
2020-11-10 13:54:11 +0100

    bug(payment): payment modal bug

--- src/Helpers/Helper.js
+++ src/Helpers/Helper.js
@@ -435,7 +435,7 @@ export function disableKey(e) {
 }
 
 export function filterUserCards(info) {
-    console.log(info.authorization.data)
+    // updated from '.info.authorization' to .info.authorization.data'
     if (info && info.authorization) {
         return info.authorization.data.filter((content) => content.channel === 'card');
     }
